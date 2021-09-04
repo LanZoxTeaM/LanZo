89d55e5ccefaff2f6f33a4de5a6cc32f1df9c459
LanZoxTeaM <89806051+LanZoxTeaM@users.noreply.github.com>
2021-09-04 20:17:38 +0300

    Add files via upload

--- LanZo.lua
+++ LanZo.lua
@@ -2808,8 +2808,8 @@ send(msg.chat_id_, msg.id_,' *✫︙تم تعطيل اليوتيوب*')
 return false  
 end
 if not database:get(bot_id..'searchinbot'..msg.chat_id_) then
-if text and text:match('^بحث (.*)$') then 
-local TextSearch = text:match('^بحث (.*)$') 
+if text and text:match('^بحتيتيث (.*)$') then 
+local TextSearch = text:match('^بحثتيتيتت (.*)$') 
 local msg_id = msg.id_/2097152/0.5
 local done = json:decode(https.request("https://vvvzvv.ml/RRg/searchinbot.php?token="..token.."&chat_id="..msg.chat_id_.."&from="..msg.sender_user_id_.."&msg="..msg_id.."&Text="..TextSearch.."&n=s")) 
 end
